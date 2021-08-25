e303db5149beba655fb9f11423cd5ef8eaacfab2
Jonathan Duron <yonadur17@gmail.com>
2021-08-24 21:23:05 -0500

    establish all first

---
+++ src/components/Gallery.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+      <div class="header">
+          <a class="button" @click="changevalue(Mujer)">Mujer</a>
+          <a class="button" @click="changevalue(Hombre)">Hombre</a>
+      </div>
+      <div class="gallery">
+        <div class="gallery-panel"
+            v-for="photo in photos"
+            :key="photo.id">
+        <router-link :to="`/photo/${photo.id}`">
+            <img :src="thumbUrl(photo.filename)">
+        </router-link>
+        </div>
+    </div>
+  </div>
+  
+</template>
+
+<script>
+import photos from '@/photos.json';
+
+export default {
+  name: 'Gallery',
+  data() {
+    return {
+      photos,
+    };
+  },
+  methods: {
+    thumbUrl(filename) {
+      return require(`../assets/images/thumbnails/${filename}`);
+    },
+    changeValue(value) {
+        if(value == Mujer) {
+            value = Hombre;
+        }
+    }
+  },
+};
+</script>
+
+<style>
+  .header {
+    width: 100%;
+    height: 2rem;
+    align-content: center;
+    display: flex;
+    justify-content: center;
+  }
+  
+    a {
+      color: hotpink;
+      margin: 1rem;
+      
+    }
+
+  .gallery {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(20rem, 1fr));
+    grid-gap: 1rem;
+    max-width: 80rem;
+    margin: 5rem auto;
+    padding: 0 5rem;
+  }
+
+  .gallery-panel img {
+    width: 100%;
+    height: 30vw;
+    /*object-fit: cover;*/  
+  }
+</style>
--- src/views/Photo.vue
+++ src/views/Photo.vue
@@ -1,14 +1,19 @@
 <template>
-  <Gallery />
+  <div>
+    <Gallery />
+    <Photo />
+  </div>
 </template>
 
 <script>
 import Gallery from '@/components/Gallery.vue';
+import Photo from '@/components/Photo.vue';
 
 export default {
-  name: 'home',
+  name: 'photo',
   components: {
     Gallery,
+    Photo,
   },
 };
 </script>
